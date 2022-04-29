0f9eedb5a734d69c990988ef1f9334779bb48a4a
Administrator <admin@example.com>
2022-04-29 10:07:51 +0800

    falsk get value from url

--- views.py
+++ views.py
@@ -1,11 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint(__name__, 'views')
 
 
 @views.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", name="horse")
 
 
-
+@views.route("/profile/<username>")  # access path from url path
+def profile(username):
+    args = request.args
+    field = args.get('field')  # access path field value by post through url
+    return render_template("index.html", name=username, field=field)
